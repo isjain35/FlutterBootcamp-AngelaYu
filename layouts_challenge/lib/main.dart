8138687544f7856d946f1d6d336f6cd845325a2e
Ishan Jain <isjain35@gmail.com>
2020-05-24 00:50:45 +0530

    Layout challenge.

---
+++ layouts_challenge/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
