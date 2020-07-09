52eb56a015cc6c2db2daa68a369493cef06ddc87
Ishan Jain <isjain35@gmail.com>
2020-07-09 23:18:30 +0530

    A weather forcasting app using API.

---
+++ clima_flutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:clima_flutter/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
